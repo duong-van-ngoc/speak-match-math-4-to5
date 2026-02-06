a606bd4be25af5941acb8336a47561696b8e1fb7
lilu <lilu49329@gmail.com>
2026-02-06 14:24:43 +0700

    insert new game

---
+++ speak-3/src/scenes/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Scenes Index - Export tất cả scenes để main.ts import dễ dàng
+ */
+
+// Base scenes (giữ ở root)
+export { default as SceneBase } from './SceneBase';
+export { default as PreloadScene } from './PreloadScene';
+
+// Game scenes (trong thư mục con)
+// export { default as SpeakScene } from './speak/SpeakScene';
+// export { default as UnderlineCharScene } from './underline/UnderlineCharScene';
+
+export { default as EndGameScene } from './end/EndScene';
+export { default as BootScene } from './BootScene';
